a932cc8a3353e19bdac774476bac746eb7d94a94
Thanh Tung <hothanhtung12@gmail.com>
2021-07-18 16:03:38 +0700

    thanhtung

--- Basic/js/main.js
+++ Basic/js/main.js
@@ -1,6 +1,37 @@
+const open = document.querySelector('.open')
+const close = document.querySelector('.close')
+const menuMobile = document.querySelector('.header__menu-mobile-content')
+
+open.onclick = function () {
+    menuMobile.classList.add('open')
+}
+close.onclick = function () {
+    menuMobile.classList.remove('open')
+}
+
+// Sroll Navbar
+const headers = document.querySelector('.header__nav')
+const logoBlack = document.querySelector('.logo-black')
+const logoWhite = document.querySelector('.logo-white')
+
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if (window.scrollY > headers.offsetHeight + 40) {
+        headers.classList.add('active')
+        logoBlack.style.display = 'block'
+        logoWhite.style.display = 'none'
+    }else{
+        headers.classList.remove('active')
+        logoBlack.style.display = 'none'
+        logoWhite.style.display = 'block'
+    }
+}
+
+
+// // BACK TO TOP
 const backToTop = document.querySelector('.backtoTop')
 
-// BACK TO TOP
 document.querySelector('.backtoTop').addEventListener('click' , scrollToTop)
 window.addEventListener('scroll', () =>{
     backToTop.classList.toggle('active' , window.scrollY > 100)
@@ -11,4 +42,3 @@ function scrollToTop() {
         behavior: "smooth",
     })
 }
-
